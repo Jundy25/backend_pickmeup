b7e4290c22891daf70417a21970f74f95d9c12da
Jundy25 <127575259+Jundy25@users.noreply.github.com>
2024-08-09 02:36:38 +0800

    api changes

--- pickmeup1/routes/api.php
+++ pickmeup1/routes/api.php
@@ -16,19 +16,24 @@ use Laravel\Sanctum\Sanctum;
 //USER MANAGEMENT
 Route::prefix('/user')->group(function() {
     Route::post('/login', [AuthController::class, 'loginAccount']);
+    Route::post('/loginMob', [AuthController::class, 'loginAccountMobile']);
     Route::post('/signup', [AuthController::class, 'createAccount']);
-    Route::get('/me', [AuthController::class, 'show'])->middleware(['auth:sanctum']);
     Route::patch('/me', [AuthController::class, 'accountUpdate'])->middleware(['auth:sanctum']);
     Route::post('/logout', [AuthController::class, 'logoutAccount'])->middleware(['auth:sanctum']);
     Route::get('/rider', [AuthController::class, 'showRider']);
     Route::get('/customer', [AuthController::class, 'showCustomer']);
     Route::get('/dashboard/counts', [DashboardController::class, 'getCounts']);
     Route::get('/riders', [RiderController::class, 'getRiders']);
-    Route::get('/customers', [CustomerController::class, 'getCustomers']);
-    Route::get('/admin', [AdminController::class, 'getAdmin']);
-    Route::put('admin/{user_id}/status', [AdminController::class, 'updateStatus']);
-    Route::put('customer/{user_id}/status', [CustomerController::class, 'updateStatus']);
     Route::put('rider/{user_id}/status', [CustomerController::class, 'updateStatus']);
+
+    Route::get('/customers', [CustomerController::class, 'getCustomers']);
+    Route::put('customer/{user_id}/status', [CustomerController::class, 'updateStatus']);
+
+    Route::get('/admin', [AdminController::class, 'getAdmin']);
+    Route::get('/admin/{id}', [AdminController::class, 'show']);
+    Route::put('admin/{user_id}/status', [AdminController::class, 'updateStatus']);
+    Route::put('/update_admin/{id}', [AdminController::class, 'updateAdmin']);
+    
     Route::get('/history', [HistoryController::class, 'index']);
     Route::get('/feedbacks', [FeedbackController::class, 'index']);
 });
